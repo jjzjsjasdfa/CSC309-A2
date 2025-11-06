4123dfc580181fee92b38f8a8bce702406e41b0c
mark <mark.lu@mail.utoronto.ca>
2025-11-06 15:43:55 -0500

    Fix routes

--- src/routes/eventsRoutes.js
+++ src/routes/eventsRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const eventsController = require("../controllers/eventsController");
 
-const { authenticateToken, authorization, validatePayload, verifyUserId, debug } = require('../middleware/middleware');
+const { authenticateToken, authorization,organizerAuthorization, validatePayload, verifyUserId, debug } = require('../middleware/middleware');
 
 router.route("/")
 .post(
@@ -21,6 +21,31 @@ router.route("/")
     }, "query"),
     eventsController.getEvents
 )
-    
+
+router.route("/:eventId/organizers")
+.post(
+    authenticateToken,            
+    authorization(["manager", "superuser"]),  
+    validatePayload({ 
+        required: ["utorid"],
+    }, "body"),
+    eventsController.organizeEvent
+)
+
+router.route("/:eventId/organizers/:userId")
+.delete(
+    authenticateToken,            
+    authorization(["manager", "superuser"]),
+    eventsController.kickOrganizer
+)
+
+router.route("/:eventId")
+.delete(
+    authenticateToken, 
+    authorization(["manager", "superuser"]),
+    eventsController.deleteEvent)
+.get(authenticateToken, 
+    authorization(["regular","cashier","manager", "superuser"]),
+    eventsController.getEvent)
 
 module.exports = router;
